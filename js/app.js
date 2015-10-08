5634be2e5b70efeb004268e443021f3b4b6fa72f
jonathanlie94 <jonathanlie94@gmail.com>
2015-10-08 18:59:35 +0800

    feature: Player and Enemy's movements
    Added event handlers for player movements. Note that the players' movements are not bound to the grid provided in the map, instead it is a smooth movement.The same goes with the enemy's movements as well.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(row, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.startingRow = row;
+    this.speed = speed;
+    this.x = 0;
+    this.y = row * 83.0;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -14,6 +18,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.outOfBounds()) {
+        // reset its position
+        this.x = 0;
+        this.y = this.startingRow * 83.0;
+    }
+    this.x += dt * this.speed;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,16 +31,69 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Check if the enemy is out of canvas' bounds
+Enemy.prototype.outOfBounds = function() {
+    if (this.x > ctx.canvas.width || this.y > ctx.canvas.height) {
+        return true;
+    }
+};
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.x = 2 * 101.0;
+    this.y = 5 * 83.0;
+    this.keyStates = {
+        'left': false,
+        'up': false,
+        'right': false,
+        'down': false
+    };
+    this.sprite = 'images/char-boy.png';
+};
 
+Player.prototype.update = function() {
+    var self = this;
+    var movementSpeed = 2;
+    if (self.keyStates.left) {
+        self.x -= movementSpeed;
+    }
+    if (self.keyStates.up) {
+        self.y -= movementSpeed;
+    }
+    if (self.keyStates.right) {
+        self.x += movementSpeed;
+    }
+    if (self.keyStates.down) {
+        self.y += movementSpeed;
+    }
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(eventType, keyCode) {
+    if (eventType === 'keydown') {
+        this.keyStates[keyCode] = true;
+    }
+    else if (eventType === 'keyup') {
+        this.keyStates[keyCode] = false;
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var enemy1 = new Enemy(1, 100);
+var enemy2 = new Enemy(2, 50);
+var enemy3 = new Enemy(3, 200);
+var allEnemies = [enemy1, enemy2, enemy3];
 
+var player = new Player();
+//ctx.addEventListener('click', player.handleInput, false);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -42,5 +105,24 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput('keyup', allowedKeys[e.keyCode]);
+});
+
+// This 'move' event is triggered when 'keydown' event is fired.
+//
+document.addEventListener('move', function(e) {
+     var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput('keydown', allowedKeys[e.keyCode]);
+});
+
+document.addEventListener('keydown', function(e) {
+    var evt = new Event('move');
+    evt.keyCode = e.keyCode;
+    document.dispatchEvent(evt);
 });
